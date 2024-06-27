3b5a6bcc65f6c7669f06ed504d2f8778d7f98de2
Arun <arunpuram@outlook.com>
2024-06-27 12:14:19 +0530

    Add files via upload

---
+++ Linkedin_connector_bot.py
@@ -0,0 +1,10 @@
+#############################################
+__author__ = "Arun Puram"                  #
+__LinkedIn__ = "Arun Puram"                #
+__Github__ = "Afadeofred"                  #
+#############################################
+
+
+from Scripts import graphical_script
+if __name__ == '__main__':
+    graphical_script.guibuild()
